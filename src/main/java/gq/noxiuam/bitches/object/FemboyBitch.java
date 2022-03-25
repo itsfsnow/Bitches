a21642be411d1ff7bc8bdfa5a662964e59ddd418
Freddy Snow <itsfsnow@gmail.com>
2022-03-25 12:38:31 +0000

    Added femboy bitch

---
+++ src/main/java/gq/noxiuam/bitches/object/FemboyBitch.java
@@ -0,0 +1,17 @@
+package gq.noxiuam.bitches.object;
+
+import lombok.*;
+
+// Basically what 90% of Lunar Client's discord is
+@Getter
+public class FemboyBitch extends Bitch {
+
+    private final boolean fatherless, degenerate;
+    private final int hotness = 1000;
+
+    public EmoBitch(boolean fatherless, boolean degenerate) {
+        this.fatherless = fatherless;
+        this.degenerate = degenerate;
+        this.createNewBitch();
+    }
+}
