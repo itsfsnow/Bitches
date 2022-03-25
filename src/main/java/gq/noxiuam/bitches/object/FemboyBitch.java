17d788ec987c3faf661df13cf5a4429023784fae
itsfsnow <itsfsnow@gmail.com>
2022-03-25 19:11:10 +0000

    Fixed constructor

--- src/main/java/gq/noxiuam/bitches/object/FemboyBitch.java
+++ src/main/java/gq/noxiuam/bitches/object/FemboyBitch.java
@@ -2,14 +2,14 @@ package gq.noxiuam.bitches.object;
 
 import lombok.*;
 
-// Basically what 90% of Lunar Client's discord is
+// Basically what 99% of Lunar Client's discord is
 @Getter
 public class FemboyBitch extends Bitch {
 
     private final boolean fatherless, degenerate;
     private final int hotness = 1000;
 
-    public EmoBitch(boolean fatherless, boolean degenerate) {
+    public FemboyBitch(boolean fatherless, boolean degenerate) {
         this.fatherless = fatherless;
         this.degenerate = degenerate;
         this.createNewBitch();
